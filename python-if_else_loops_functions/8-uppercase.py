3528d1dd26143c0d5a252a03f1331c536f6c4e3d
SpStigma <githubstigma@gmail.com>
2023-09-26 09:50:36 +0200

    First attempt of task 9.

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if 97 <= ord(char) <= 122:
+            uppercase_char = chr(ord(char) - 32)
+            print(uppercase_char, end='')
+        else:
+            print(char, end='')
+print()
