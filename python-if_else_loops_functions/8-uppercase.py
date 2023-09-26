777bc3ee521d73d784ad9f1800ed4042e3bd1c00
SpStigma <githubstigma@gmail.com>
2023-09-26 10:04:38 +0200

    fix: add .format.

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for char in str:
         if 97 <= ord(char) <= 122:
             uppercase_char = chr(ord(char) - 32)
-            print(uppercase_char, end='')
+            print('{}'.format(uppercase_char), end='')
         else:
-            print(char, end='')
+            print('{}'.format(char), end='')
 print()
