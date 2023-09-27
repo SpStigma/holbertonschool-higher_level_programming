bb0a6f01302b4c8c41011bdfdf0b1f2a397eb4b0
SpStigma <githubstigma@gmail.com>
2023-09-27 09:26:22 +0200

    Fix: spelling mistake.

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,5 @@
 import hidden_4
 
 if __name__ == "__main__":
-    hidden_module = [attr for attr in dir(hidden_4) if not attr.startswitch('__')]
+    hidden_module = [attr for attr in dir(hidden_4) if not attr.startswith('__')]
     print(hidden_module)
-    
