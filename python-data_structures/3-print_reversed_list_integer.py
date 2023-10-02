1b1f1bceb40561f5d7c59f7644b625a5e7555684
SpStigma <githubstigma@gmail.com>
2023-10-02 15:31:53 +0200

    Fix: new way to print reverse list.

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    reverse_list = my_list[::-1]
-    for i in range(len(reverse_list)):
-        print("{:d}".format(reverse_list[i]))
+    my_list.reverse()
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
