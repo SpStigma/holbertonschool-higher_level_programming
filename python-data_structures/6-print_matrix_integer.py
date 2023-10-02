a4996e0056d883fd6ffd43a20d8387cb670865d5
SpStigma <githubstigma@gmail.com>
2023-10-02 19:20:40 +0200

    Fix: end= to the print.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for ligne in matrix:
         for element in ligne:
-            print("{:d}".format(element), end=" ")
+            print("{:d}".format(element), end="")
         if element != ligne[-1]:
             print(" ", end="")
         print()
