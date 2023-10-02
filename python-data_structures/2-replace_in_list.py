e89c130974e13cb478673df68f9f6496c7807170
SpStigma <githubstigma@gmail.com>
2023-10-02 14:56:13 +0200

    First attempt of 2. Replace element

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    lenght = len(my_list)
+    if idx < 0 or idx > lenght - 1:
+        return my_list
+
+    my_list[idx] = element
+    return my_list
+    
