6e52329a749111d688974fc123a63e0f653091b2
SpStigma <githubstigma@gmail.com>
2023-10-02 19:23:14 +0200

    Fix: indexation of if.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
     for ligne in matrix:
         for element in ligne:
             print("{:d}".format(element), end="")
-        if element != ligne[-1]:
-            print(" ", end="")
+            if element != ligne[-1]:
+                print(" ", end="")
         print()
