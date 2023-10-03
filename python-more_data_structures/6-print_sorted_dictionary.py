964d0db1defb3df19c44e372f05920419c9c4578
SpStigma <githubstigma@gmail.com>
2023-10-03 14:27:41 +0200

    Fix: Try to change .item to .keys.

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.items()):
-        print("{} : {}".format(key, value))
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
+        print("{} : {}".format(key, a_dictionary[key]))
