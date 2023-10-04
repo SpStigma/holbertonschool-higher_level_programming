6b5d07fab604250fdc569f9056575a477d61f53d
SpStigma <githubstigma@gmail.com>
2023-10-04 19:32:36 +0200

    First attempt of 0. Safe list printing.

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    number_of_count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            number_of_count += 1
+        except IndexError:
+            break
+    print()
+    return number_of_count
