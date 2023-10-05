8453c0c942d0a50f9b56961fc57b5f6e51e069ed
SpStigma <githubstigma@gmail.com>
2023-10-05 15:26:21 +0200

    Fix: condition is only a string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if roman_string is None and isinstance(roman_string, str):
+    if roman_string is None:
         return 0
+    if roman_string is isinstance(roman_string, str):
+        return None
     dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     try:
         total = 0
