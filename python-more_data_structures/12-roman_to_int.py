77dd9392ef09eef2e0d1c1a74d87821e91ae903d
SpStigma <githubstigma@gmail.com>
2023-10-05 15:15:35 +0200

    First attempt 12. Roman to Integer.

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    try:
+        total = 0
+        for i in range(len(roman_string)):
+            if dict[roman_string[i]] < dict[roman_string[i + 1]]:
+                total -= dict[roman_string[i]]
+            else:
+                total += dict[roman_string[i]]
+    except IndexError:
+        total += dict[roman_string[i]]
+    return total
