2e378d1378fbe93b1dab77fb31b348f45dd0e80e
SpStigma <githubstigma@gmail.com>
2023-10-09 18:42:34 +0200

    Fix: change emplacement my_print to last.

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -37,14 +37,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def my_print(self):
-        if self.__size == 0:
-            print()
-        for i in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
-
     @property
     def position(self):
         return self.position
@@ -57,3 +49,11 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
