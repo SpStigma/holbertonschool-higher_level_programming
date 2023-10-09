bb628c9f2a6f9f688679682d2891e8d8cdbf7260
SpStigma <githubstigma@gmail.com>
2023-10-09 10:22:54 +0200

    First attempt 3. Area of a square.

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" This script define a square with a private instance attribute."""
+
+
+class Square:
+    """ A class that represent a square. Who use the method of __init__
+    to make a private attribute who has some condition for size and one methode
+    to calculate the area."""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
