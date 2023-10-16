406555d8bfa17efbdef8e74341488af7d55ec38d
SpStigma <githubstigma@gmail.com>
2023-10-16 22:12:08 +0200

    Fix: add some docstring.

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 
+""" Module 0-lookup """
+
+
 def lookup(obj):
+    """
+    Function that return the list of available attributes
+    and methods of an object
+
+    Args:
+        obj: object to check
+
+    Returns:
+        the list of available attributes and methods of an object
+    """
     return dir(obj)
