5ea57e1931ee17095d7fd9c24774640795984293
SpStigma <githubstigma@gmail.com>
2023-10-17 17:08:26 +0200

    Fix: remove the close case and add end at print.

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -28,5 +28,4 @@ def read_file(filename=""):
     """
     with open(filename, 'r', encoding="utf-8") as file:
         read_data = file.read()
-    print(read_data)
-    file.close()
+    print(read_data, end='')
