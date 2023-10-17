7a26bcf9b5e81d97701add9e0c0454b7fa3fcb1f
SpStigma <githubstigma@gmail.com>
2023-10-17 10:12:45 +0200

    First attempt of 6. Improve Geometry.

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+""" 5-base_geometry """
+
+
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+
+    This class currently doesn't have any specific functionality
+    and is intended to be used as a base for more specialized geometry classes.
+
+    Methods:
+        (None)
+    """
+    pass
+
+    def area(self):
+        """
+        Calculate the area of géometry (not implemented).
+
+        Raises:
+            Exception: Indicates that the method is not implemented.
+
+        Returns:
+            None
+        """
+        raise Exception("area() is not implemented")
