070d7329196ef601725c0d6b062a20ff933e617e
SpStigma <githubstigma@gmail.com>
2023-10-17 13:13:21 +0200

    Fix: docstring.

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,6 +4,28 @@
 
 
 class MyList(list):
+    """
+    Extends the built-in list class with additional functionality.
+
+    This class inherits from the built-in list class and adds a custom method
+    called `print_sorted` to print the elements of the list in sorted order.
+
+    Attributes:
+        Inherits all attributes and methods from the built-in list class.
+
+    Methods:
+        print_sorted(self):
+            Print the elements of the list in sorted order.
+
+            This method sorts the elements of the list in ascending order and
+            prints them.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     def print_sorted(self):
         """
         Print the elements of the list in sorted order.
