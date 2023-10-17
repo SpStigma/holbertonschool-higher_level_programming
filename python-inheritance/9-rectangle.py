5371b5402314f56d4c4dfab5d06ed0e0b20fc72d
SpStigma <githubstigma@gmail.com>
2023-10-17 14:49:20 +0200

    First attempt of 9. Full rectangle.

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -41,6 +41,30 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
+        """
+    Calculate the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle, which is the product of
+            its width and height.
+
+        Example:
+            >>> rect = Rectangle(5, 7)
+            >>> rect.area()
+        35
+        """
         return self.__height * self.__width
+
     def __str__(self):
+        """
+        Return a string representation of the rectangle.
+
+        Returns:
+            str: A string in the format "[Rectangle] <width>/<height>".
+
+        Example:
+            >>> rect = Rectangle(5, 7)
+            >>> str(rect)
+            '[Rectangle] 5/7'
+        """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
