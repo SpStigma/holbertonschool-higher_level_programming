136d2f782cd6ea5914b0f506dd4230d4a00aa32a
SpStigma <githubstigma@gmail.com>
2023-10-17 17:03:51 +0200

    First attempt of 0. Read file.

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" 0-read_file module """
+
+
+def read_file(filename=""):
+    """
+    Reads and prints the contents of a text file.
+
+    Args:
+        filename (str): The name of the file to be read.
+        Defaults to an empty string.
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFoundError: If the specified file is not found.
+
+        UnicodeDecodeError: If an error occurs while decoding
+        the file with utf-8 encoding.
+
+    Usage:
+        >>> read_file('example.txt')
+        This is an example text file.
+        It contains multiple lines.
+        Each line is separated by a newline character.
+    """
+    with open(filename, 'r', encoding="utf-8") as file:
+        read_data = file.read()
+    print(read_data)
