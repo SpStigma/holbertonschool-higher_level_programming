2f3c988cff6af8f81183173dc1be47c1e3cd557e
SpStigma <githubstigma@gmail.com>
2023-10-17 09:34:32 +0200

    First attempt of 4. Only sub class of.

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" module 4-inherits_from """
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if an object inherits from a specified class.
+
+    Parameters:
+        obj (object): The object to be checked.
+        a_class (class): The class to check for inheritance.
+
+    Returns:
+        bool: True if the object inherits from the
+        specified class, False otherwise.
+    """
+    return issubclass(type(obj), a_class)
