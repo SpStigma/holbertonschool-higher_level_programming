75259432ae3b2fe2a3e931dbe7496556c349c248
SpStigma <githubstigma@gmail.com>
2023-10-17 10:00:39 +0200

    First attempt of 5. Geometry module.

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" 5-base_geometry """
+
+
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+
+    This class currently doesn't have any specific functionality
+    and is intended to be used as a base for more specialized geometry classes.
+
+    Methods:
+        (None)
+    """
+    pass
