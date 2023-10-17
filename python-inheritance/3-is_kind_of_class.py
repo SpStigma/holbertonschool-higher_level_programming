36385f4aed1cec547970a9f169dc52b83a50522d
SpStigma <githubstigma@gmail.com>
2023-10-17 09:04:59 +0200

    Fix: docstring.

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,4 +4,15 @@
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    Check if an object is an instance of a specified class or a subclass of it.
+
+    Args:
+        obj (object): The object to be checked.
+        a_class (class): The class to check against.
+
+    Returns:
+    True if the object is an instance of the specified class or a subclass of it,
+    False otherwise.
+    """
     return isinstance(obj, a_class)
