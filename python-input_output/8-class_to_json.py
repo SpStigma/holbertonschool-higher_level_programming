622f4d9630ab35ebfcde028e0d12962984b5014a
SpStigma <githubstigma@gmail.com>
2023-10-18 13:53:32 +0200

    First attempt of 8. Class to JSON.

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""8-class_to_json module"""
+
+
+def class_to_json(obj):
+    """
+    Converts a Python object to a JSON serializable dictionary.
+
+    Args:
+        obj (object): The Python object to be converted.
+
+    Returns:
+        dict: A dictionary that can be serialized to JSON.
+
+    Raises:
+        (Include any specific exceptions this function might raise)
+
+    Examples:
+        >>> class_to_json({'key': 'value'})
+        {'key': 'value'}
+
+    Note:
+        (Any additional notes about the function)
+    """
+    return obj.__dic___
