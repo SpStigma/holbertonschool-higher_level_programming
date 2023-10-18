99f06b2cead6fb636311be48137fdac56330f524
SpStigma <githubstigma@gmail.com>
2023-10-18 14:10:56 +0200

    First attempt of 9. Student to JSON.

---
+++ python-input_output/9-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""9-student module """
+
+
+class Student:
+    """
+    Represents a student.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student object.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+
+        Returns:
+            None
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Convert the Student object to a dictionary.
+
+        Returns:
+            dict: A dictionary containing the student's information.
+        """
+        return self.__dict__
