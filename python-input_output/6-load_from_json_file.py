93d781c2a4ad2d9342614f10e1bd3497c7ce0797
SpStigma <githubstigma@gmail.com>
2023-10-18 12:54:28 +0200

    First attempt of 6. Create object from a JSON file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" 6-load_from_json_file module """
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Load JSON data from a file and return the parsed content.
+
+    Args:
+        filename (str): The name of the file to load JSON data from.
+
+    Returns:
+        dict: The parsed JSON data.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        json.JSONDecodeError: If there is an error in decoding JSON data.
+
+    Example:
+        >>> data = load_from_json_file('example.json')
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        data = json.loads(file)
+        return data
