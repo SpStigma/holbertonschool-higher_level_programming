6f616b828cf0ad03c2971a1f5431616db5cf6611
SpStigma <githubstigma@gmail.com>
2023-10-18 13:58:46 +0200

    Fix: add a 't' to dic.

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -12,15 +12,5 @@ def class_to_json(obj):
 
     Returns:
         dict: A dictionary that can be serialized to JSON.
-
-    Raises:
-        (Include any specific exceptions this function might raise)
-
-    Examples:
-        >>> class_to_json({'key': 'value'})
-        {'key': 'value'}
-
-    Note:
-        (Any additional notes about the function)
     """
-    return obj.__dic__
+    return obj.__dict__
