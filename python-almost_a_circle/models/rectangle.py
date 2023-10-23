12c5b03bbe9284c03f369ac1f47b635ab510618f
SpStigma <githubstigma@gmail.com>
2023-10-23 10:43:16 +0200

    First attempt of 2. First Rectangle.

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python3
+
+""" rectangle module """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Represents a rectangle with specified dimensions and position.
+
+    Args:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        x (int, optional): The x-coordinate of the
+        top-left corner. Default is 0.
+        y (int, optional): The y-coordinate of the
+        top-left corner. Default is 0.
+        id (int, optional): The unique identifier for
+        the rectangle. Default is None.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        x (int): The x-coordinate of the top-left corner.
+        y (int): The y-coordinate of the top-left corner.
+        id (int): The unique identifier for the rectangle.
+
+    Inherits from:
+        Base: A base class providing a unique identifier.
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a Rectangle instance with
+        specified dimensions and position.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the
+            top-left corner. Default is 0.
+            y (int, optional): The y-coordinate of the
+            top-left corner. Default is 0.
+            id (int, optional): The unique identifier for
+            the rectangle. Default is None.
+        """
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """
+        Get the width of the rectangle.
+
+        Returns:
+            int: The width of the rectangle.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set the width of the rectangle.
+
+        Args:
+            value (int): The new width of the rectangle.
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Get the height of the rectangle.
+
+        Returns:
+            int: The height of the rectangle.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Set the height of the rectangle.
+
+        Args:
+            value (int): The new height of the rectangle.
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        Get the x-coordinate of the top-left corner.
+
+        Returns:
+            int: The x-coordinate.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Set the x-coordinate of the top-left corner.
+
+        Args:
+            value (int): The new x-coordinate.
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        Get the y-coordinate of the top-left corner.
+
+        Returns:
+            int: The y-coordinate.
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Set the y-coordinate of the top-left corner.
+
+        Args:
+            value (int): The new y-coordinate.
+        """
+        self.__y = value
