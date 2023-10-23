c110a1f1b6890f007134f52d42d4a9fe330c1483
SpStigma <githubstigma@gmail.com>
2023-10-23 09:40:09 +0200

    First attempt of 1. Base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,35 @@
+#!usr/bin/python
+
+"""base module"""
+
+
+class Base:
+    """
+    Base class for creating objects with unique IDs.
+
+    Attributes:
+        __nb_objects (int): A class variable to keep
+        track of the number of instances created.
+
+    Args:
+        id (int, optional): An optional argument to specify
+        a custom ID. If not provided, a unique ID will be assigned.
+
+    Attributes:
+        id (int): The unique identifier for each instance.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initializes a new instance of the Base class.
+
+        Args:
+            id (int, optional): An optional argument to specify a custom ID.
+            If not provided, a unique ID will be assigned
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
