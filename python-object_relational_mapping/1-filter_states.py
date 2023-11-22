50a6edfb10fbc47e138e89052bdb6f627e0ec097
SpStigma <yoann.rivet0@gmail.com>
2023-11-22 16:41:40 +0100

    Fix: change order by Binary in the clause where.

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -17,8 +17,7 @@ if __name__ == "__main__":
     )
 
     cursor = database.cursor()
-    cursor.execute("""SELECT * FROM states WHERE name LIKE 'N%'
-                   ORDER BY states.id""")
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
 
     for row in cursor.fetchall():
         print(row)
