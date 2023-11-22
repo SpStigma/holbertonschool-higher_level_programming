fa8ac7f8d1bb5576b8f6dc1b90147063c4c3e020
SpStigma <yoann.rivet0@gmail.com>
2023-11-22 16:33:51 +0100

    First attempt of Filter states.

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Script that lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+
+    cursor = database.cursor()
+    cursor.execute("""SELECT * FROM states WHERE name LIKE 'N%'
+                   ORDER BY states.id""")
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    database.close()
