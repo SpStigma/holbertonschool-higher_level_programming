ccff77e995ff2cca2ab588c9b6198b0635a05096
SpStigma <yoann.rivet0@gmail.com>
2023-11-23 10:10:10 +0100

    Fix: add a commentary on class.

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -22,6 +22,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Commentaire class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
