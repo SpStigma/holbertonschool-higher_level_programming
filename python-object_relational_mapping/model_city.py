fb50f27f73d08ec4198f1737c7a2d7db55350ddd
SpStigma <yoann.rivet0@gmail.com>
2023-11-24 16:03:21 +0100

    First attempt of Cities in state.

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Script to define a SQLAlchemy model for a 'states' table in a MySQL database.
+
+This script defines a SQLAlchemy model named 'State', which represents a table
+named 'states' in a MySQL database. The table has two columns: 'id' as the
+primary key (an integer that cannot be null) and 'name' as a string with a
+maximum length of 128 characters (cannot be null). The script creates an
+SQLAlchemy Engine to connect to a MySQL database and uses it to create the
+'states' table.
+
+Make sure to replace the database connection details (username, password,
+database name) in the 'create_engine' line before running the script.
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base
+from model_state import Base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """City class"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id', ondelete='CASCADE'), nullable=False)
